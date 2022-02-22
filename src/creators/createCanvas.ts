7f1bde27a55cdcc3a136c1b462a5869b946cf571
j19015 <j19015@sangi.jp>
2022-02-22 14:10:35 +0900

    add letter object

--- src/creators/createCanvas.ts
+++ src/creators/createCanvas.ts
@@ -14,5 +14,21 @@ export function createCanvas(
   h: number
 ): HTMLCanvasElement {
   // レターオブジェクトを生成します。
+  const canvas = document.createElement("canvas");
+  canvas.setAttribute("width", w + "px");
+  canvas.setAttribute("height", h + "px");
 
+  const context = canvas.getContext("2d");
+  if (!context) {
+    throw new Error();
+  }
+
+  context.fillStyle = "white";
+  context.font = fontSize + "px " + FONT_BASE;
+  context.textAlign = "center";
+  context.textBaseline = "top";
+
+  context.fillText(label, w / 2, 0);
+
+  return canvas;
 }
